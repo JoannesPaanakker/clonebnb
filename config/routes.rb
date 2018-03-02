95d8c00bd173cf8decb88f7712c11b825061b2ed
Joannes Paanakker <J.Paanakker@upcmail.nl>
2018-03-02 16:26:00 +0100

    not too much

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boats do
     resources :bookings, only: [:show, :new, :create]
+    member do
+      get 'cost', to: 'bookings#cost', as: :bookingcost
+    end
   end
   resources :bookings, only: [:edit, :update]
   devise_for :users, controllers: { sessions: "sessions" }
